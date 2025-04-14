2e698f047867ff0ead55716410dea70f8c992532
Anurag Singh <anuragsingh2313@gmail.com>
2025-04-14 10:15:09 +0530

    updated

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,5 +2,5 @@ import { prismaClient } from "@repo/database/client";
 
 export default async function Home() {
   const user = await prismaClient.user.findFirst();
-  return <div>{user?.username}</div>;
+  return <div>username: {user?.username}</div>;
 }
